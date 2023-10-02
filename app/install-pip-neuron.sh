a2cc3030d4dab05ef0f22894cec879c0ededb1a4
yahavb <yahavb@amazon.com>
2023-10-02 12:58:14 -0700

    docker public.ecr.aws/ubuntu/ubuntu:23.10

--- app/install-pip-neuron.sh
+++ app/install-pip-neuron.sh
@@ -1,6 +1,6 @@
 #!/bin/bash -x
 
-PIP="python3.8 -m pip"
+PIP="pip"
 if [ "$(uname -i)" = "x86_64" ]; then
   ${PIP} config set global.extra-index-url $PIP_REPO 
   ${PIP} install --force-reinstall torch-neuronx==1.13.0.* neuronx-cc==2.* --extra-index-url $PIP_REPO
