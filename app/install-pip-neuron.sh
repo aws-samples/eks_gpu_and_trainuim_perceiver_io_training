1271709982da3adf70d61d1eaa2146ff8f6362eb
yahavb <yahavb@amazon.com>
2023-10-03 08:13:21 -0700

    neuron

--- app/install-pip-neuron.sh
+++ app/install-pip-neuron.sh
@@ -4,5 +4,5 @@ PIP="pip"
 if [ "$(uname -i)" = "x86_64" ]; then
   ${PIP} config set global.extra-index-url $PIP_REPO 
   #${PIP} install --force-reinstall torch-neuronx neuronx-cc --extra-index-url $PIP_REPO --break-system-packages
-  ${PIP} install --force-reinstall neuronx-cc==2.* torch-neuronx torchvision --extra-index-url $PIP_REPO --break-system-packages
+  ${PIP} install --force-reinstall neuronx-cc torch-neuronx torchvision --extra-index-url $PIP_REPO --break-system-packages
 fi
