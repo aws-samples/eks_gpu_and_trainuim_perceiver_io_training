a1868c88b1a2bfa91cd6525fe54df9fcfd18408c
yahavb <yahavb@amazon.com>
2023-10-04 15:32:47 -0700

    install-drivers-and-tools-nvidia-arm64

--- app/install-drivers-and-tools-nvidia.sh
+++ app/install-drivers-and-tools-nvidia.sh
@@ -12,7 +12,7 @@ if [ "$(uname -i)" = "x86_64" ]; then
   echo "deb [signed-by=/usr/share/keyrings/cuda-wsl-ubuntu-keyring.gpg] https://developer.download.nvidia.com/compute/cuda/repos/wsl-ubuntu/x86_64/ /" | tee /etc/apt/sources.list.d/cuda-wsl-ubuntu-x86_64.list 
   wget https://developer.download.nvidia.com/compute/cuda/repos/wsl-ubuntu/x86_64/cuda-wsl-ubuntu.pin 
   mv cuda-wsl-ubuntu.pin /etc/apt/preferences.d/cuda-repository-pin-600
-  apt-get update -y && apt-get install -y cuda
+  apt-get update -y && apt-get install -y cuda nvidia-cuda-toolkit
 fi
 if [ "$(uname -i)" = "aarch64" ]; then
   wget https://developer.download.nvidia.com/compute/cuda/repos/wsl-ubuntu/x86_64/cuda-wsl-ubuntu-keyring.gpg 
@@ -20,5 +20,5 @@ if [ "$(uname -i)" = "aarch64" ]; then
   echo "deb [signed-by=/usr/share/keyrings/cuda-wsl-ubuntu-keyring.gpg] https://developer.download.nvidia.com/compute/cuda/repos/ubuntu2204/arm64/ /" | tee /etc/apt/sources.list.d/cuda-wsl-ubuntu-arm64.list 
   wget https://developer.download.nvidia.com/compute/cuda/repos/ubuntu2204/arm64/cuda-ubuntu2204.pin
   mv cuda-ubuntu2204.pin /etc/apt/preferences.d/cuda-repository-pin-600
-  apt-get update -y && apt-get install -y cuda
+  apt-get update -y && apt-get install -y cuda-cross-aarch64
 fi
