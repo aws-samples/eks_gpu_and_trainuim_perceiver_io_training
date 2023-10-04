8854e69ac87f9cb3a2a8651cd8659ffcccf12bd5
yahavb <yahavb@amazon.com>
2023-10-04 11:56:03 -0700

    ubuntu2204

---
+++ app/install-drivers-and-tools.sh
@@ -0,0 +1,28 @@
+#!/bin/bash -x
+
+if [ "$(uname -i)" = "x86_64" ]; then
+  # Configure Linux for Neuron repository updates
+  . /etc/os-release
+  tee /etc/apt/sources.list.d/neuron.list > /dev/null <<EOF
+  deb https://apt.repos.neuron.amazonaws.com ${VERSION_CODENAME} main
+EOF
+  wget -qO - https://apt.repos.neuron.amazonaws.com/GPG-PUB-KEY-AMAZON-AWS-NEURON.PUB | apt-key add -
+
+  # Update OS packages 
+  apt-get update -y
+
+  # Install OS headers 
+  apt-get install linux-headers-$(uname -r) -y
+
+  # Install git 
+  apt-get install git -y
+
+  # install Neuron Driver
+  apt-get install aws-neuronx-dkms=2.* -y
+
+  # Install Neuron Tools 
+  apt-get install aws-neuronx-tools=2.* -y
+
+  # Add PATH
+  export PATH=/opt/aws/neuron/bin:$PATH
+fi
---
+++ app/install-pytorch-neuron.sh
@@ -0,0 +1,25 @@
+#!/bin/bash
+
+if [ "$(uname -i)" = "x86_64" ]; then
+  # Install Python venv 
+  apt-get install -y python3.10-venv g++ 
+
+  # Create Python venv
+  python3.10 -m venv aws_neuron_venv_pytorch_inf1 
+
+  # Activate Python venv 
+  . aws_neuron_venv_pytorch_inf1/bin/activate 
+  python -m pip install -U pip 
+
+  # Install Jupyter notebook kernel
+  pip install ipykernel 
+  python3.10 -m ipykernel install --user --name aws_neuron_venv_pytorch_inf1 --display-name "Python (torch-neuron)"
+  pip install jupyter notebook
+  pip install environment_kernels
+
+  # Set pip repository pointing to the Neuron repository 
+  python -m pip config set global.extra-index-url https://pip.repos.neuron.amazonaws.com
+
+  # Install PyTorch Neuron
+  python -m pip install torch-neuron neuron-cc[tensorflow] "protobuf" torchvision
+fi
