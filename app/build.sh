528eb10e3e3579314214c4b5e95c43fa1f140015
yahavb <yahavb@amazon.com>
2023-10-05 14:30:11 -0700

    test-dl-container

--- app/build.sh
+++ app/build.sh
@@ -6,7 +6,8 @@
 
 BASE_IMAGE=$AWS_ACCOUNT_ID.dkr.ecr.$AWS_REGION.amazonaws.com/$BASE_REPO:$BASE_IMAGE_TAG
 aws ecr get-login-password --region us-east-1 | docker login --username AWS --password-stdin 763104351884.dkr.ecr.us-east-1.amazonaws.com
-docker build -t $BASE_IMAGE .
+docker pull 763104351884.dkr.ecr.us-east-1.amazonaws.com/pytorch-training:2.0.1-gpu-py310-cu121-ubuntu20.04-ec2
 docker logout 763104351884.dkr.ecr.us-east-1.amazonaws.com
 aws ecr get-login-password --region $AWS_REGION | docker login --username AWS --password-stdin $BASE_IMAGE
+docker build -t $BASE_IMAGE .
 docker push $BASE_IMAGE
