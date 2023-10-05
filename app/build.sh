00cb0e94d1f081b63ce8cb704cdf09c98df81aa2
yahavb <yahavb@amazon.com>
2023-10-05 14:24:34 -0700

    test-dl-container

--- app/build.sh
+++ app/build.sh
@@ -7,5 +7,6 @@
 BASE_IMAGE=$AWS_ACCOUNT_ID.dkr.ecr.$AWS_REGION.amazonaws.com/$BASE_REPO:$BASE_IMAGE_TAG
 aws ecr get-login-password --region us-east-1 | docker login --username AWS --password-stdin 763104351884.dkr.ecr.us-east-1.amazonaws.com
 docker build -t $BASE_IMAGE .
+docker logout 763104351884.dkr.ecr.us-east-1.amazonaws.com
 aws ecr get-login-password --region $AWS_REGION | docker login --username AWS --password-stdin $BASE_IMAGE
 docker push $BASE_IMAGE
