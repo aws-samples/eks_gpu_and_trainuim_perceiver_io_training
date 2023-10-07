b709f5441fd98d414cd79126bc9d0076577d2a7b
yahavb <yahavb@amazon.com>
2023-10-07 16:07:57 -0700

    xla-cuda

--- app/build.sh
+++ app/build.sh
@@ -2,6 +2,8 @@
 
 BASE_IMAGE=$AWS_ACCOUNT_ID.dkr.ecr.$AWS_REGION.amazonaws.com/$BASE_REPO:$BASE_IMAGE_TAG
 IMAGE=$AWS_ACCOUNT_ID.dkr.ecr.$AWS_REGION.amazonaws.com/$BASE_REPO:$IMAGE_TAG
+env
+which envsubst
 cat Dockerfile.template | envsubst > Dockerfile
 cat Dockerfile
 aws ecr get-login-password --region $AWS_REGION | docker login --username AWS --password-stdin $BASE_IMAGE
