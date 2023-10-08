a1d929495f61590170262217628ef2a96cf919ea
yahavb <yahavb@amazon.com>
2023-10-08 12:13:41 -0700

    gpu

--- app/build.sh
+++ app/build.sh
@@ -2,10 +2,7 @@
 
 export BASE_IMAGE=$AWS_ACCOUNT_ID.dkr.ecr.$AWS_REGION.amazonaws.com/$BASE_REPO:$BASE_IMAGE_TAG
 export IMAGE=$AWS_ACCOUNT_ID.dkr.ecr.$AWS_REGION.amazonaws.com/$BASE_REPO:$IMAGE_TAG
-env
-which envsubst
 cat Dockerfile.template | envsubst > Dockerfile
-cat Dockerfile
 aws ecr get-login-password --region $AWS_REGION | docker login --username AWS --password-stdin $BASE_IMAGE
 docker build -t $IMAGE .
 docker push $IMAGE
