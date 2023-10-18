ae2069318e86d35cda7bb84a148a6dc8d4386487
yahavb <yahavb@amazon.com>
2023-10-18 13:18:25 -0700

    restore var_tmp_neuron-compile-cache

--- app/prep_dataset.sh
+++ app/prep_dataset.sh
@@ -15,7 +15,7 @@ mkdir -p /root/.torch/vision/datasets/kinetics/
 aws s3 cp s3://deepmind-kinetics/1723303957.pt /root/.torch/vision/datasets/kinetics/
 aws s3 cp s3://deepmind-kinetics/c785d04824.pt /root/.torch/vision/datasets/kinetics/
 mkdir -p /var/tmp/neuron-compile-cache
-aws s3 cp s3://deepmind-kinetics/var_tmp_neuron-compile-cache.tar.gz /var/tmp/neuron_compile_cache
+aws s3 cp s3://deepmind-kinetics/var_tmp_neuron-compile-cache.tar.gz /var/tmp/neuron-compile-cache
 cd /var/tmp/neuron-compile-cache
 tar -xvzf var_tmp_neuron-compile-cache.tar.gz
 cd $DATASET_DIR/train
